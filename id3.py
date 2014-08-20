e7475c609d5cda172cfa7947441f3475649e1e8b
jmu303 <muj@bc.edu>
2014-08-20 02:42:40 -0400

    fix id3 logic bug, now correctly constructs tree
    previously wasn't filtering subsets of subsets, only subsets of data,
    leading to incorrect gain calculations

--- id3.py
+++ id3.py
@@ -7,7 +7,6 @@ import dtree
 import csv
 import math
 from collections import Counter
-import pdb
 
 
 class ID3(object):
@@ -15,41 +14,46 @@ class ID3(object):
         self.filename = filename
         self.dtree = None
 
+    def learn(self):
         self.parse_csv()
         self.get_distinct_values()
-
         self.create_tree()
 
-    def create_tree(self, parent=None, parent_value=None):
+    def create_tree(self, subset=None, parent=None, parent_value=None):
         """
         Recursively create the decision tree.
 
         OPTIMIZE: Possibly can be optimized by setting root node at beginning
         """
 
-        if parent_value == 'Sunny':
-            pdb.set_trace()
+        # pdb.set_trace()
 
         # Identify the subset of the data used in the igain calculation
-        if parent is None:
+        if subset is None:
+            # Initial run, use all data
             subset = self.data
         else:
-            subset = self.get_subset(parent.attribute, parent_value)
+            # Filter the subset down for the given parent attributes
+            subset = self.filter_subset(subset, parent.attribute, parent_value)
 
         counts = self.attr_counts(subset, self.dependent)
         # If every element in the subset belongs to one dependent group, label
         # with that group.
         if len(counts) == 1:  # Only one value of self.dependent detected
-            print "ALL ARE ONE"
-            print counts
-            node = dtree.DTree(attribute=counts.keys()[0], leaf=True)
+            node = dtree.DTree(
+                attribute=counts.keys()[0],
+                leaf=True,
+                parent_value=parent_value
+            )
         elif not self.remaining_attributes:  # OPTIMIZE by setting attribute
             # Also, if there are no remaining attributes, label with the most
             # common attribute in the subset.
-            print "FINDING MOST COMMON"
-            print counts
             most_common = max(counts, key=lambda k: counts[k])
-            node = dtree.DTree(attribute=most_common, leaf=True)
+            node = dtree.DTree(
+                attribute=most_common,
+                leaf=True,
+                parent_value=parent_value
+            )
         else:
             igains = []
             for attr in self.remaining_attributes:
@@ -65,14 +69,19 @@ class ID3(object):
             )
 
         if parent is None:
+            node.set_attributes(self.attributes)
             self.dtree = node
+            # Set known order of attributes for learning
         else:
             parent.add_child(node)
 
         if not node.leaf:  # Continue recursing
             for value in self.values[node.attribute]:
-                print "TESTING FOR {0}".format(value)
-                self.create_tree(parent=node, parent_value=value)
+                self.create_tree(
+                    subset=subset,
+                    parent=node,
+                    parent_value=value
+                )
 
     def parse_csv(self, dependent_index=-1):
         """
@@ -120,14 +129,14 @@ class ID3(object):
             return self.attributes
         return [a for a in self.attributes if a not in self.dtree.attributes]
 
-    def get_subset(self, attr, value):
+    def filter_subset(self, subset, attr, value):
         """
         Return a subset of the rows of the training data where the given
         attribute has the given value. Used in the recursive construction of
         the decision tree.
 
         """
-        return [r for r in self.data if r[attr] == value]
+        return [r for r in subset if r[attr] == value]
 
     def information_gain(self, subset, attr):
         gain = self.get_base_entropy(subset)
@@ -174,6 +183,14 @@ class ID3(object):
         for row in subset:
             counts[row[attr]] += 1
         return counts
+
+    def __str__(self):
+        """
+        String representation of the ID3 algorithm assumes user simply wants
+        the tree representation.
+
+        """
+        return str(id3.dtree)
 
 
 if __name__ == '__main__':
